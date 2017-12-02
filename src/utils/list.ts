46f22915637b0d5ea2b5effd0548f90dba62d7c1
int0h <int0h@yandex.ru>
2017-12-03 00:36:47 +0300

    List via hash table

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,73 +1,31 @@
-export interface ListItem<T> {
-    id: number;
-    data: T;
+export interface IdDict<T> {
+    [key: number]: T;
 }
 
 // todo redo with hash table
 export class List<T> {
-    items: ListItem<T>[];
-    toDelete: Set<number>;
+    private items: IdDict<T>;
     currentId = 0;
 
     add(item: T): number {
         const id = this.currentId;
         this.currentId++;
-        this.items.push({id, data: item});
+        this.items[id] = item;
         return id;
     }
 
     del(id: number) {
-        this.toDelete.add(id);
-
-        onNextTick(this.clean);
+        delete this.items[id];
     }
 
     get(id: number) {
-        return this.items.filter(i => i.id === id)[0].data;
-    }
-
-    clean = () => {
-        if (this.toDelete.size < 1) {
-            return;
-        }
-
-        let deletedCount = 0;
-        let newItems: ListItem<T>[] = [];
-
-        for (let item of this.items) {
-            if (this.toDelete.has(item.id)) {
-                deletedCount++;
-                continue;
-            }
-            newItems.push(item);
-        }
-
-        if (this.toDelete.size !== deletedCount) {
-            throw new Error('Bad list item ID');
-        }
-
-        this.toDelete = new Set<number>();
-        this.items = newItems;
+        return this.items[id];
     }
 
     entries(): [number, T][] {
-        this.clean();
-
-        return this.items.map(item => [item.id, item.data]) as [number, T][];
+        return Object.keys(this.items).map(key => {
+            const id = Number(key);
+            return [id, this.items[id]] as [number, T];
+        });
     }
-}
-
-let nextTickTaskList: Function[] = [];
-
-function onNextTick(task: () => void) {
-    if (nextTickTaskList.length === 0) {
-        setTimeout(() => {
-            for (let task of nextTickTaskList) {
-                task();
-            }
-            nextTickTaskList = [];
-        }, 0);
-    }
-
-    nextTickTaskList.push(task);
 }
