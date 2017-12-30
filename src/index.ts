dd6b31e4bfa5b8f69ff779c980d798fcb89aa046
int0h <int0h@yandex.ru>
2017-12-31 00:14:32 +0300

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export {HyperValue} from './core';
-// export {hvAuto, hvMake, hvBind, hvWrap, hvCast, hvOnceOf} from './helpers/tools';
-// export {HvScope} from './helpers/hvScope';
-// export {BaseScope} from './scopes/base';
+
 import * as scopes from './scopes';
 export {scopes};
-// export {HvArray} from './helpers/array';
